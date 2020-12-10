c628817fd80094510dce89b1614199c1906e22fa
Diego <dardila90@gmail.com>
2020-12-09 19:48:34 -0500

    inicializacion del proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, { useEffect } from 'react';
+import Axios from 'axios';
+//1220261362
 function App() {
+
+  useEffect(() => {
+    Axios({
+      method:"POST",
+      url:"https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/877980/comments?access_token=frM5ZnpYKBwYXLR4BO7P39xR6LsNIhv8o3ESAZIo21v91IGnFCw&comment=Excelente banda, lo mejor del rock en Colombia!!",
+    }).then(data => console.log(data))
+      .catch(err => console.log(err))
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
